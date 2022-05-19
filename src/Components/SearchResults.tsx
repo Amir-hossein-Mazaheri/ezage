4e800a1716e73d67e0590448b178f251732adf4b
LightSpeedGuy <uplayggboy@gmail.com>
2022-05-20 00:57:53 +0430

    search result component based on image card

---
+++ src/Components/SearchResults.tsx
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import Loading from "../Common/Loading";
+import SearchContext from "../Context/SearchContext";
+import ImageCard from "./ImageCard";
+
+const SearchResults: React.FC = () => {
+  const {
+    searchSlice: { results, isSearching },
+  } = useContext(SearchContext);
+
+  if (isSearching) {
+    return <Loading />;
+  }
+
+  console.log(results);
+
+  return (
+    <View style={styles.searchResults}>
+      <FlatList
+        data={results}
+        keyExtractor={(item) => (item as { id: string }).id}
+        renderItem={({ item }) => (
+          <ImageCard
+            id={item.id}
+            title={item.alt_description}
+            url={item.urls.small}
+            description={item.description}
+            likes={item.likes}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchResults: {
+    marginVertical: 15,
+    paddingHorizontal: 15,
+    flex: 1,
+  },
+});
+
+export default SearchResults;
