4f026eabddb6a8fd7e53af4222afca8624fffae5
LightSpeedGuy <uplayggboy@gmail.com>
2022-05-20 20:10:03 +0430

    fetch next page results and append them into existing one when we reach the end of list

--- src/Components/SearchResults.tsx
+++ src/Components/SearchResults.tsx
@@ -1,13 +1,35 @@
-import React, { useContext } from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import React, { useCallback, useContext } from "react";
+import { View, StyleSheet, FlatList, Text } from "react-native";
+import unsplashSearch from "../Api/unsplashSearch";
 import Loading from "../Common/Loading";
-import SearchContext from "../Context/SearchContext";
+import SearchContext, {
+  addToResults,
+  incrementPage,
+  setNextPageSearchingStatus,
+} from "../Context/SearchContext";
 import ImageCard from "./ImageCard";
 
 const SearchResults: React.FC = () => {
   const {
-    searchSlice: { results, isSearching },
+    searchSlice: { results, isSearching, page, query, isSearchingForNextPage },
+    dispatch,
   } = useContext(SearchContext);
+
+  const getNextPage = useCallback(async () => {
+    dispatch(setNextPageSearchingStatus({ status: true }));
+    dispatch(incrementPage());
+    try {
+      const { data } = await unsplashSearch.get("/", {
+        params: { query, page: page + 1 },
+      });
+
+      dispatch(setNextPageSearchingStatus({ status: false }));
+      dispatch(addToResults({ results: data.results, count: data.total }));
+    } catch (err) {
+      console.log(err, "\n");
+      console.log(err.response, "\n");
+    }
+  }, [dispatch, page, query]);
 
   if (isSearching) {
     return <Loading />;
@@ -18,6 +40,9 @@ const SearchResults: React.FC = () => {
       <FlatList
         data={results}
         keyExtractor={(item) => (item as { id: string }).id}
+        onEndReached={getNextPage}
+        ListFooterComponent={isSearchingForNextPage && <Text>Loading...</Text>}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <ImageCard
             id={item.id}
@@ -27,7 +52,6 @@ const SearchResults: React.FC = () => {
             likes={item.likes}
           />
         )}
-        showsVerticalScrollIndicator={false}
       />
     </View>
   );
