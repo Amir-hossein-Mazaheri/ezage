3c06e9df6195952a641b6947528fbd2e7a90ce5a
LightSpeedGuy <uplayggboy@gmail.com>
2022-05-20 20:27:59 +0430

    added react native progress bar into loading

--- src/Common/Loading.tsx
+++ src/Common/Loading.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import * as Progress from "react-native-progress";
+
 const Loading = () => {
   return (
     <View style={styles.loading}>
-      <Text>Loading...</Text>
+      <Progress.CircleSnail
+        size={50}
+        color={"white"}
+        style={{ marginBottom: 12 }}
+      />
+      <Text style={styles.loadingText}>Loading...</Text>
     </View>
   );
 };
@@ -12,9 +19,14 @@ const Loading = () => {
 const styles = StyleSheet.create({
   loading: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgb(0,0,0, 0.3)",
+    backgroundColor: "rgba(0,0,0, 0.7)",
     alignItems: "center",
     justifyContent: "center",
+  },
+  loadingText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
