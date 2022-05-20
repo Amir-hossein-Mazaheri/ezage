239b849880891ebc6fd927d6e8b01f6e0e11d337
LightSpeedGuy <uplayggboy@gmail.com>
2022-05-20 21:03:02 +0430

    fetch some default photo on mounting

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,9 +1,34 @@
-import * as React from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { View, StyleSheet } from "react-native";
+import unsplash from "../Api/unsplash";
+
 import SearchBar from "../Components/SearchBar";
 import SearchResults from "../Components/SearchResults";
+import SearchContext, {
+  applyResults,
+  setSearchingStatus,
+  toggleOnInit,
+} from "../Context/SearchContext";
 
 const Home = () => {
+  const { dispatch } = useContext(SearchContext);
+  const dispatchRef = useRef(dispatch);
+
+  useEffect(() => {
+    dispatchRef.current(setSearchingStatus({ status: true }));
+    (async () => {
+      try {
+        const { data } = await unsplash.get("/");
+        dispatchRef.current(setSearchingStatus({ status: false }));
+        dispatchRef.current(applyResults({ results: data, count: 10 }));
+        dispatchRef.current(toggleOnInit());
+      } catch (err) {
+        console.log(err, "\n");
+        console.log(err.response, "\n");
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <SearchBar />
