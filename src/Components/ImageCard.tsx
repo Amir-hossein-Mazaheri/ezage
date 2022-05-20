320ebe9bf70a10fc4ad507cf26364aa2c6370123
LightSpeedGuy <uplayggboy@gmail.com>
2022-05-20 23:51:06 +0430

    moved style into seperate file

--- src/Components/ImageCard.tsx
+++ src/Components/ImageCard.tsx
@@ -1,16 +1,10 @@
 import React, { useCallback, useContext, useRef } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  Pressable,
-} from "react-native";
+import { View, Text, Image, TouchableOpacity, Pressable } from "react-native";
 
 import { Entypo } from "@expo/vector-icons";
 import { EvilIcons } from "@expo/vector-icons";
 import SearchContext, { incrementLike } from "../Context/SearchContext";
+import styles from "./ImageCartStyle";
 
 interface ImageCardProps {
   id: string;
@@ -73,62 +67,5 @@ const ImageCard: React.FC<ImageCardProps> = ({
     </Pressable>
   );
 };
-
-const styles = StyleSheet.create({
-  imageCard: {
-    elevation: 10,
-    shadowColor: "#ccc",
-    marginBottom: 20,
-    borderRadius: 12,
-    flex: 1,
-    backgroundColor: "white",
-    overflow: "hidden",
-  },
-  details: {
-    paddingTop: 5,
-    paddingBottom: 7,
-    paddingHorizontal: 12,
-  },
-  imageTitle: {
-    fontSize: 15,
-    fontWeight: "bold",
-    marginBottom: 6,
-    textTransform: "capitalize",
-  },
-  imageDescription: {
-    marginBottom: 8,
-  },
-  image: {
-    maxWidth: "100%",
-    height: 165,
-    marginBottom: 8,
-  },
-  bottomBody: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 8,
-  },
-  detailsButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 16,
-    borderRadius: 999,
-    backgroundColor: "#3498db",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 5,
-  },
-  detailsButtonText: {
-    color: "white",
-    fontSize: 12,
-    fontWeight: "bold",
-    marginLeft: 5,
-  },
-  likes: {
-    flexDirection: "row",
-    alignSelf: "flex-end",
-    marginBottom: 8,
-  },
-});
 
 export default React.memo(ImageCard);
