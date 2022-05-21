90880aa0805f8ec62e5affbf3c31f412ee0ea597
LightSpeedGuy <uplayggboy@gmail.com>
2022-05-21 17:47:59 +0430

    added some types and poppins font

--- App.tsx
+++ App.tsx
@@ -3,20 +3,49 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_600SemiBold,
+  Poppins_400Regular,
+} from "@expo-google-fonts/poppins";
 import ProductionConfig from "./src/Helpers/ProductionConfig";
 import Home from "./src/Screens/Home";
 import StoreProvider from "./src/Context/StoreProvider";
+import SingleImage from "./src/Screens/SingleImage";
+import Loading from "./src/Common/Loading";
+import toTitleCase from "./src/Helpers/toTitleCase";
 
-const RootStack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  SingleImage: {
+    id: string;
+    title: string;
+  };
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 const prod = new ProductionConfig();
 
 prod.clearLogs();
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+    Poppins_600SemiBold,
+    Poppins_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <StoreProvider>
       <NavigationContainer>
+        {/* @ts-ignore */}
         <RootStack.Navigator initialRouteName="Home">
+          {/* @ts-ignore */}
           <RootStack.Group
             screenOptions={{
               headerStyle: {
@@ -24,15 +53,33 @@ const App: React.FC = () => {
               },
               headerTitleAlign: "center",
               headerTitleStyle: {
+                fontFamily: "Poppins_600SemiBold",
                 color: "#fff",
-                // fontFamily: "Poppins",
               },
+              headerBackButtonMenuEnabled: false,
             }}
           >
             <RootStack.Screen
               name="Home"
               options={{ title: "Image Search" }}
               component={Home}
+            />
+            <RootStack.Screen
+              name="SingleImage"
+              options={({ route }) => {
+                let title = route.params.title;
+                if (!title) {
+                  title = "No Title";
+                }
+                const improvedTitle = toTitleCase(title);
+                return {
+                  title:
+                    improvedTitle.length <= 15
+                      ? improvedTitle
+                      : improvedTitle.slice(0, 15) + " ...",
+                };
+              }}
+              component={SingleImage}
             />
           </RootStack.Group>
         </RootStack.Navigator>
